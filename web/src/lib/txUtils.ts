787eb576719c4b9e779c75f5266e154ae2fcf68c
aidan46 <aidansullivan.dev@gmail.com>
2025-07-31 16:13:16 +0700

    feat: improve timeAgo formatting to include days ago

--- web/src/lib/txUtils.ts
+++ web/src/lib/txUtils.ts
@@ -22,11 +22,13 @@ export function formatFunctionName(signature: string): string {
 
 export function timeAgo(timestamp: string): string {
 	const now = Date.now();
-	const seconds = now / 1000 - parseInt(timestamp, 10);
+	const seconds = Math.floor(now / 1000 - parseInt(timestamp, 10));
 	const minutes = Math.floor(seconds / 60);
 	const hours = Math.floor(minutes / 60);
-	if (minutes < 1) return "just now";
-	if (minutes < 60) return `${minutes} min${minutes > 1 ? "s" : ""} ago`;
-	if (hours < 24) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
-	return new Date(parseInt(timestamp, 10) * 1000).toLocaleString();
+	const days = Math.floor(hours / 24);
+
+	if (seconds < 60) return "just now";
+	if (minutes < 60) return `${minutes} min${minutes !== 1 ? "s" : ""} ago`;
+	if (hours < 24) return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
+	return `${days} day${days !== 1 ? "s" : ""} ago`;
 }
